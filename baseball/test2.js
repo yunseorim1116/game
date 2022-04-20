e1340bd0b06ee0d28e1b72d01c6541a9b30a24cd
seorim <tjfla6417@naver.com>
2022-04-20 15:23:23 +0900

    change color n add feature

--- baseball/test2.js
+++ baseball/test2.js
@@ -1,4 +1,3 @@
-
 const playNumber = 4;
 
 let set = new Set();
@@ -9,10 +8,10 @@ console.log(randNumArr);
 
 let chanceNumber = 10;
 let count = 0;
-let gameSwitch = true;
 let strike = 0;
 let ball = 0;
 let out = 0;
+let outCheck = false;
 
 const userNumber = document.querySelector(".user-number");
 const inputValBtn = document.querySelector(".inputval-btn");
@@ -22,7 +21,7 @@ let strikeHTML = document.querySelector(".strike");
 let ballHTML = document.querySelector(".ball");
 let outHTML = document.querySelector(".out");
 const userInput = document.querySelector(".user-input");
-
+const userNum = document.querySelector(".show-user-num");
 
 userChanceText.innerText = chanceNumber - count;
 
@@ -49,7 +48,6 @@ function compareNumber() {
     userNumber.value = "";
     userNumber.focus();
 
-
     if (count == 10) {
       document.querySelector(".game-lose").classList.remove("hidden");
       document.querySelector(".game-hint-box").classList.add("hidden");
@@ -59,6 +57,7 @@ function compareNumber() {
   }
 
   function whatisComNumber(userGuessNumber) {
+    outCheck = false;
     for (let i = 0; i < 4; i++) {
       if (randNumArr[i] == userGuessNumber[i]) {
         strike++;
@@ -71,11 +70,46 @@ function compareNumber() {
       }
     }
 
-    gameHint();
+    makeUserNumHtml(userGuessNumber);
+    gameHint(userGuessNumber);
   }
 
-  function gameHint() {
+  function makeUserNumHtml(userGuessNumber) {
+    const divAllBox = document.createElement("div");
+    divAllBox.classList.add("userNumAllbox");
+    divAllBox.id = `userChoice${count}`;
+    for (let x of userGuessNumber) {
+      const divBox = document.createElement("div");
+      divBox.classList.add("userNumbox");
+
+      divBox.innerText = x;
+      divAllBox.appendChild(divBox);
+      userNum.insertBefore(divAllBox, userNum.firstChild); //맨 앞에 삽입
+    }
+  }
+
+  function gameHint(userGuessNumber) {
+    const userChoiceNumber = userGuessNumber.split("");
+
+    userChoiceNumber.forEach((userNumber) => {
+      for (let j = 0; j < userChoiceNumber.length; j++) {
+        if (userNumber == randNumArr[j]) {
+          outCheck = true;
+        }
+      }
+    });
+
+    console.log(outCheck);
+    !outCheck
+      ? outHTML.classList.remove("hidden")
+      : outHTML.classList.add("hidden");
+
     if (strike == playNumber) {
+      const changeColorGreen = document.querySelectorAll(".userNumbox");
+      changeColorGreen.forEach((ele) => {
+        ele.style.backgroundColor = "green";
+      });
+
       homeruneHTML = "HomeRun ~!!";
       userChanceText.innerText = "0";
       document.querySelector(".game-win").classList.remove("hidden");
@@ -83,12 +117,21 @@ function compareNumber() {
       userInput.classList.add("hidden");
 
       return;
+    } else if (!outCheck) {
+      console.log(count);
+      const changeColorRed = document.getElementById(`userChoice${count}`);
+      const outBox = changeColorRed.childNodes;
+      outBox.forEach((ele) => {
+        ele.style.backgroundColor = "rgb(255, 75, 75)";
+      });
     } else {
-      strikeHTML.innerText = `${strike}스트라이크`;
-      ballHTML.innerText = `${ball}볼`;
+      strikeHTML.innerText = `${strike} 스트라이크`;
+      ballHTML.innerText = `${ball} 볼`;
 
       strike = 0;
       ball = 0;
     }
+
+    //다른 방법 생각해본건strile,ball이 0이며
   }
 }
